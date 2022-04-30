6affdebb6057e9c41ca84610b0fad48693d6d6e8
Ng Yen Kai <yngg0058@student.monash.edu>
2022-05-01 02:27:09 +0800

    REQ7: begin implementation

--- src/game/Player.java
+++ src/game/Player.java
@@ -4,80 +4,88 @@ import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.displays.Display;
+import edu.monash.fit2099.engine.displays.Menu;
 import edu.monash.fit2099.engine.items.Item;
 import edu.monash.fit2099.engine.positions.GameMap;
-import edu.monash.fit2099.engine.displays.Menu;
-
 import java.util.List;
 
 /**
  * Class representing the Player.
  */
-public class Player extends Actor  {
+public class Player extends Actor {
 
-	private final Menu menu = new Menu();
-	private final ConsumedItemManager consumedItemManager;
+    private final Menu menu = new Menu();
+    private final ConsumedItemManager consumedItemManager;
+	private boolean didReset;
 
 	/**
-	 * Constructor.
-	 *
-	 * @param name        Name to call the player in the UI
-	 * @param displayChar Character to represent the player in the UI
-	 * @param hitPoints   Player's starting number of hitpoints
-	 */
-	public Player(String name, char displayChar, int hitPoints) {
-		super(name, displayChar, hitPoints);
-		this.addCapability(Status.HOSTILE_TO_ENEMY);
-		consumedItemManager = ConsumedItemManager.getInstance();
-	}
+     * Constructor.
+     *
+     * @param name        Name to call the player in the UI
+     * @param displayChar Character to represent the player in the UI
+     * @param hitPoints   Player's starting number of hitpoints
+     */
+    public Player(String name, char displayChar, int hitPoints) {
+        super(name, displayChar, hitPoints);
+        this.addCapability(Status.HOSTILE_TO_ENEMY);
+        consumedItemManager = ConsumedItemManager.getInstance();
+    }
 
-	@Override
-	public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
-		// Handle multi-turn Actions
-		if (lastAction.getNextAction() != null)
+    @Override
+    public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
+        // Handle multi-turn Actions
+		if (lastAction.getNextAction() != null) {
 			return lastAction.getNextAction();
-
-
-		//Ticker for ConsumedItemManager
-		consumedItemManager.consumedItemTicker();
-		//If player has super mushroom effect ongoing, change the display character to M
-		if (this.hasCapability(Status.SUPER_MUSHROOM_EFFECT_ONGOING)){
-			this.setDisplayChar('M');
-		}
-		else if  (!this.hasCapability(Status.SUPER_MUSHROOM_EFFECT_ONGOING)){
-			this.setDisplayChar('m');
-		}
-		// Print player's hp
-		System.out.println("Player's HP: " + printHp());
-		// Print wallet balance
-		System.out.println("Wallet: $" + WalletManager.getInstance().getWalletBalance(this));
-
-
-		for(Item item: getInventory()){
-		System.out.println(item.toString());}
-
-		////For testing. Check actor's capabilities every turn////
-		System.out.println("Actor's capabilities: ");
-		List<Enum<?>> status = this.capabilitiesList();
-		for (Enum<?> stat: status){
-			System.out.println(stat.toString());
 		}
 
-		// return/print the console menu
-		return menu.showMenu(this, actions, display);
+        //Ticker for ConsumedItemManager
+        consumedItemManager.consumedItemTicker();
+        //If player has super mushroom effect ongoing, change the display character to M
+        if (this.hasCapability(Status.SUPER_MUSHROOM_EFFECT_ONGOING)) {
+            this.setDisplayChar('M');
+        } else if (!this.hasCapability(Status.SUPER_MUSHROOM_EFFECT_ONGOING)) {
+            this.setDisplayChar('m');
+        }
+        // Print player's hp
+        System.out.println("Player's HP: " + printHp());
+        // Print wallet balance
+        System.out.println("Wallet: $" + WalletManager.getInstance().getWalletBalance(this));
+
+        for (Item item : getInventory()) {
+            System.out.println(item.toString());
+        }
+
+        ////For testing. Check actor's capabilities every turn////
+        System.out.println("Actor's capabilities: ");
+        List<Enum<?>> status = this.capabilitiesList();
+        for (Enum<?> stat : status) {
+            System.out.println(stat.toString());
+        }
+
+        // return/print the console menu
+        return menu.showMenu(this, actions, display);
+    }
+
+    @Override
+    public char getDisplayChar() {
+        return this.hasCapability(Status.TALL) ? Character.toUpperCase(super.getDisplayChar())
+                : super.getDisplayChar();
+    }
+
+	public void markResetAsDone() {
+		didReset = true;
 	}
 
-	@Override
-	public char getDisplayChar(){
-		return this.hasCapability(Status.TALL) ? Character.toUpperCase(super.getDisplayChar()): super.getDisplayChar();
-	}
+    @Override //This returns allowableAction.
+    public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
 
-	@Override //This returns allowableAction.
-	public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
+        List<Item> items = this.getInventory();  //This inventory contains all items picked up
 
-		List<Item> items = this.getInventory();  //This inventory contains all items picked up
+        ActionList actions = super.allowableActions(otherActor, direction, map);
+		if (!didReset) {
+			actions.add(new ResetAction());
+		}
+        return actions;
+    }
 
-
-		return super.allowableActions(otherActor, direction, map);
-	}
 }
---
+++ src/game/ResetAction.java
@@ -0,0 +1,29 @@
+package game;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+
+public class ResetAction extends Action {
+
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        ResetManager.getInstance().run();
+        if (actor instanceof Player) {
+            ((Player) actor).markResetAsDone();
+        }
+        // TODO change uml to add ref to Player
+        return "";
+    }
+
+    @Override
+    public String menuDescription(Actor actor) {
+        return "";
+    }
+
+    @Override
+    public String hotkey() {
+        return "r";
+    }
+
+}
