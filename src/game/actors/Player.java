bccf23c7a848ca7126bdb8417a0ccbf0e6b8da81
rteh0003 <rteh0003@student.monash.edu>
2022-05-02 22:46:56 +0800

    Made changes in code

--- src/game/actors/Player.java
+++ src/game/actors/Player.java
@@ -75,12 +75,12 @@ public class Player extends Actor implements Resettable {
         System.out.println("Wallet: $" + WalletManager.getInstance().getWalletBalance(this));
 
 
-        ////For testing. Check actor's capabilities every turn////
+        /*////For testing. Check actor's capabilities every turn////
         System.out.println("Actor's capabilities: ");
         List<Enum<?>> status = this.capabilitiesList();
         for (Enum<?> stat : status) {
             System.out.println(stat.toString());
-        }
+        }*/
 
         // return/print the console menu
         return menu.showMenu(this, actions, display);
